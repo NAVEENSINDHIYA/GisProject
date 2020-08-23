b1d74dd56fb8805d82d8f14253b5d068db2d4560
Srikant Bhushan <srikant.bhusan@digitalindia.gov.in>
2020-08-23 17:31:00 +0530

    add login success handler

--- src/main/java/com/gis/springboot/cofig/SecurityConfig.java
+++ src/main/java/com/gis/springboot/cofig/SecurityConfig.java
@@ -1,5 +1,11 @@
 package com.gis.springboot.cofig;
 
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,8 +15,10 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
 @Configuration
 @EnableWebSecurity
@@ -26,13 +34,16 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 		auth.inMemoryAuthentication().withUser("user").password(encodePwd().encode("user")).roles("user")
          .and().withUser("admin").password(encodePwd().encode("admin")).roles("admin");
 	}
-	
+	@Bean
+	public AuthenticationSuccessHandler myAuthenticationSuccessHandler(){
+	    return new MyAuthenticationSuccessHandler();
+	}
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests()
         .antMatchers("/login","/webjars/**","/**/*.js", "/**/*.css","/**/*.jpg","/**/*.png").permitAll()
 		.anyRequest().authenticated().and()
-        .formLogin().loginPage("/login").and()
+        .formLogin().loginPage("/login").successHandler(myAuthenticationSuccessHandler()).and()
         .logout()
         .logoutSuccessUrl("/login").permitAll().and()
         .exceptionHandling().accessDeniedPage("/logout").and()
